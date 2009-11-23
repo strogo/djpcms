7489828eb815e9675a000eedd839a78c9175bebc
Luca Sbardella <luca.sbardella@gmail.com>
2009-11-23 13:34:36 +0000

    added ajaxhtml module

---
+++ djpcms/ajaxhtml.py
@@ -0,0 +1,40 @@
+from django.utils import simplejson as json
+
+from django.utils.safestring import mark_safe
+from django.utils.encoding import force_unicode
+
+class ajaxhtml(object):
+    
+    def __new__(cls):
+        obj = super(ajaxhtml,cls).__new__(cls)
+        d = {}
+        obj._dict = d
+        
+        # AJAX classes
+        d['post_view_key']           = 'xhr'
+        d['ajax']                    = 'ajax'
+        d['errorlist']               = 'errorlist'
+        d['formmessages']            = 'form-messages'
+        
+        # css decorators
+        d['calendar_class']          = 'calendar-input'
+        d['currency_input']          = 'currency-input'
+        d['edit']                    = 'editable'
+        d['delete']                  = 'deletable'
+        d['secondary_in_list']       = 'secondary'
+        d['link_selected']           = 'selected'
+        
+        return obj
+    
+    def __init__(self):
+        d = self._dict
+        for k,v in d.iteritems():
+            setattr(self,k,v)
+    
+    def __setitem__(self, k, v):
+        self._dict[k] = v
+
+    def tojson(self):
+        return mark_safe(force_unicode(json.dumps(self._dict)))
+    
+
