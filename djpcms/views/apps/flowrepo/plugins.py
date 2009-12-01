2195bf3f22704fb9306d046f4d964ddee44214ea
Luca Sbardella <luca.sbardella@gmail.com>
2009-12-01 23:14:47 +0000

    added references plugin to flowrepo application

--- djpcms/views/apps/flowrepo/plugins.py
+++ djpcms/views/apps/flowrepo/plugins.py
@@ -3,8 +3,9 @@
 from django.template import loader
 
 from djpcms.plugins import DJPplugin
+from djpcms.utils.html import htmlwrap
 
-from flowrepo.models import FlowItem, Report
+from flowrepo.models import FlowRelated, FlowItem, Report
 
 class YourDraft(DJPplugin):
     name = 'flowrepo-draft'
@@ -26,4 +27,36 @@ class YourDraft(DJPplugin):
             yield loader.render_to_string(['report_list_item.html',
                                            'flowrepo/report_list_item.html'],
                                            content)
+            
+
+class LinkedItems(DJPplugin):
+    '''
+    Very very useful plugin for displaying a list of linked item
+    to the flowitem object
+    '''
+    name = 'flowrepo-linked'
+    description = "Linked item urls"
+    
+    def render(self, djp, wrapper, prefix, **kwargs):
+        from djpcms.views import appsite
+        instance = djp.instance
+        if not isinstance(instance,FlowItem):
+            return
+        qs = FlowRelated.objects.filter(item = instance)
+        if not qs:
+            return
+        request = djp.request
+        return loader.render_to_string(['report_draft_list.html',
+                                        'flowrepo/report_draft_list.html'],
+                                        {'items': self.paginator(djp,qs)})
         
+    def paginator(self, djp, qs):
+        for obj in qs:
+            object  = obj.object
+            url     = object.url
+            link    = htmlwrap('a', object.name)
+            link._attrs['title'] = object.name
+            link._attrs['href'] = url
+            if not url.startswith('/'):
+                link._attrs['target'] = "_blank"
+            yield link.render()
