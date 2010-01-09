7e8b538a8634cbdd3e8a54beccbf916997bdeadb
lsbardel <luca.sbardella@gmail.com>
2010-01-09 15:45:07 +0000

    included deploy template in bits

--- djpcms/utils/cdt.py
+++ djpcms/utils/cdt.py
@@ -5,6 +5,9 @@ _tz = {'Europe/London': 136}
 
 def link(tz = None):
     tz = tz or settings.TIME_ZONE
-    n  = _tz.get(tz)
-    return 'http://www.timeanddate.com/worldclock/city.html?n=%s' % n 
+    n  = _tz.get(tz, None)
+    if n:
+        return 'http://www.timeanddate.com/worldclock/city.html?n=%s' % n
+    else:
+        return ''
     
