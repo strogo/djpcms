82ad4cc17102ce483a3f227c07aa2171e0adf5b5
lsbardel <luca.sbardella@gmail.com>
2010-09-01 23:14:38 +0100

    minor twiking in examples

--- examples/sitedjpcms/appurls.py
+++ examples/sitedjpcms/appurls.py
@@ -2,6 +2,7 @@ import os
 from django.contrib.auth.models import User
 
 from djpcms.conf import settings
+from djpcms.utils.pathtool import parentdir
 from djpcms.views import appsite
 from djpcms.views.apps.user import UserApplication
 from djpcms.views.apps.docs import DocApplication, DocView
@@ -9,13 +10,14 @@ from djpcms.views.apps.docs import DocApplication, DocView
 #from djpcms.contrib.djp_oauth.appurl import OAuthApplication
 parent = lambda x : os.path.split(x)[0]
 
-docdir = os.path.join(parent(parent(parent(os.path.abspath(__file__)))),'docs')
+
+docdir = os.path.join(parentdir(os.path.abspath(__file__),3),'docs')
 
 #_________________________________________________ Create the documentation view handler
 class DjpcmsDoc(DocApplication):
+    inherit = True
     name = 'djpcms_documentation'
     DOCS_PICKLE_ROOT = docdir
-    documentation = DocView()
     
     def get_path_args(self, lang, version):
         return ("build", "json")
